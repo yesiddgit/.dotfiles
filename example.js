9be185e988670db66e8039ac29e07f45429120eb
Yesid Garzon <yesiddgarzon@gmail.com>
2021-03-03 18:10:28 -0500

    Update example.js

--- example.js
+++ example.js
@@ -29,3 +29,26 @@ let boolean = true;
 
 console.log(typeof 42);
 console.log('An array of the prime numbers from 0 to 100: '+ primes);
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+     day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+}
