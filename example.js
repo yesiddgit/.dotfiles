13317fe52f069f85378d73116dc99521bbf6b7e0
Yesid Garzon <yesiddgarzon@gmail.com>
2021-03-03 18:14:02 -0500

    Update example.js

--- example.js
+++ example.js
@@ -52,3 +52,23 @@ switch (new Date().getDay()) {
   case 6:
     day = "Saturday";
 }
+
+function myFunction() {
+  var message, x;
+  message = document.getElementById("p01");
+  message.innerHTML = "";
+  x = document.getElementById("demo").value;
+  try {
+    if(x == "") throw "is empty";
+    if(isNaN(x)) throw "is not a number";
+    x = Number(x);
+    if(x > 10) throw "is too high";
+    if(x < 5) throw "is too low";
+  }
+  catch(err) {
+    message.innerHTML = "Error: " + err + ".";
+  }
+  finally {
+    document.getElementById("demo").value = "";
+  }
+}
