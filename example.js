895ddfd205cba92face177b310d9507eb6fc5369
Yesid Garzon <yesiddgarzon@gmail.com>
2021-03-03 18:22:04 -0500

    Update example.js

--- example.js
+++ example.js
@@ -72,3 +72,17 @@ function myFunction() {
     document.getElementById("demo").value = "";
   }
 }
+
+class ClassWithStaticMethod {
+
+  static staticProperty = 'someValue';
+  static staticMethod() {
+    return 'static method has been called.';
+  }
+
+}
+
+console.log(ClassWithStaticMethod.staticProperty);
+// output: "someValue"
+console.log(ClassWithStaticMethod.staticMethod());
+// output: "static method has been called."
