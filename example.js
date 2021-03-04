eb5d98fd490eb8de3e6b2e3d2f314fc8476a6d96
Yesid Garzon <yesiddgarzon@gmail.com>
2021-03-03 22:29:50 -0500

    Update example.js

--- example.js
+++ example.js
@@ -85,6 +85,35 @@ class ClassWithStaticMethod {
 
 let regex = /\s[A-Z]|(a-z)/g;
 
+const arrowFunciton => (a, b) => a + b + 100;
+
+const answer = x > 10 ? "greater than 10" : x < 5 ? "less than 5" : "between 5 and 10";
+
+function logArrayElements(element, index, array) {
+  console.log("a[" + index + "] = " + element);
+}
+[2, 5, 9].forEach(logArrayElements);
+
+const obj = {continent: 'Africa', country: 'Kenya', city: 'Nairobi'}
+for (let key in obj)
+  console.log(key) // output: continent, country, city
+
+calcCircumference = diameter => (
+  Math.PI * diameter;
+)
+
+let variable1;
+let variable2 = variable1  || 'bar';
+console.log(variable2 === 'bar');
+
+const odd = [1, 3, 5 ];
+const nums = [2 ,4 , 6, ...odd];
+console.log(nums); // [ 2, 4, 6, 1, 3, 5 ]
+
+// cloning arrays
+const arr = [1, 2, 3, 4];
+const arr2 = [...arr];
+
 console.log(ClassWithStaticMethod.staticProperty);
 // output: "someValue"
 console.log(ClassWithStaticMethod.staticMethod());
