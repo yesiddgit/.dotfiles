81404a9a6e0380e93750e3b54975815db55ed2ed
Yesid Garzon <yesiddgarzon@gmail.com>
2021-03-05 16:09:07 -0500

    Update example.js

--- example.js
+++ example.js
@@ -40,6 +40,586 @@ function createFile(name, temp) {
 }
 
 let obj = new Object
+//  ---------------------------------------------------------------------------
+//  BASICS
+//  ---------------------------------------------------------------------------
+
+/*  
+ *  Variables
+ *  Las variables se declaran: var nombre = contenido;
+ *  Las variables declaradas con var pueden ser usadas global o localmente según
+ *  donde se declaren. Global si es fuera de una función, local si es dentro.
+ */
+var greeter = "hey hi"; 
+
+// se pueden actualizar de dos modos:
+var greeter = "hello";
+greeter = "hello"
+
+//  ---------------------------------------------------------------------------
+//  ES6
+//  ---------------------------------------------------------------------------
+
+
+//  ---------------------------------------------------------------------------
+//  REGULAR EXPRESSIONS
+//  ---------------------------------------------------------------------------
+
+let text = "";
+let regex;
+let result;
+
+/*  
+ *  Test method -> Búsqueda
+ *  regex.test(string)
+ *  Éste método toma una expresión, aplica una cadena de texto de búsquedas
+ *  y devuelve falso o verdadero si se encuentra la cadena definida o no.
+ *
+*/
+
+text = "freeCodeCamp";
+regex = /Code/;
+result = regex.test(text);
+// console.log(result); // true
+
+/*
+ *  Match metod -> Extracción
+ *  string.match(regex)
+ *  Éste método realiza la búsqueda de una cadena de texto en una expresión y
+ *  devuelve un arreglo con las coincidencias.
+ *
+*/
+
+text = "Hello, World!";
+regex = /Hello/;
+result = text.match(regex);
+// console.log(result); // [Hello]
+
+/*  
+ *  Flags
+ *  Para ignorar las mayúsculas y las minúsculas se usa -i
+ *  Para que se encuentre cada coincidencia se usa -g
+ *
+ */
+
+text = "frEeCodeCamP";
+regex = /freecodecamp/i;
+result = text.match(regex);
+// console.log(result); // ['frEeCodeCamP']
+
+text = "Uno por uno uno";
+regex = /uno/ig;
+result = text.match(regex);
+// console.log(result); // ['Uno', 'uno', 'uno']
+
+/*  
+ *  Wildcard
+ *  El comodín (.) lo que hace es buscar cualquier caracter
+ *
+ */
+
+text = "I'll hum a bear hug";
+regex = /hu./;
+result = regex.test(result);
+//console.log(result) // true
+
+text = "Let's have fun with the run of sun";
+regex = /.un/g;
+result = text.match(regex); 
+// console.log(result);  // ['fun', 'run', 'sun']
+
+/*  
+ *  Clases de caracteres (Character Classes)
+ *  Éstas clases permiten definir un grupo o rango de caracteres en la cadena de
+ *  búsqueda (regex), colocándolos entre corchetes [].
+ *  En estas cales se pueden usar algunos caracteres para realizar búsquedas
+ *  negativas (^), caracteres al inicio (^) o al final de una cadena ($), la 
+ *  repetición de un caracter al menos una vez (+) y la repetición de un 
+ *  caracter muchas veces (*).
+ */
+
+text = "big bag bug bog";
+regex = /b[aiu]g/g;
+result = text.match(regex);
+// console.log(result);  // ['big', 'bag', 'bug']
+
+text = "big";
+regex = /b[aeo]g/;
+result = regex.test(text);
+// console.log(result); // false
+
+text = "cat bat mat";
+regex = /[a-e]at/g;
+result = text.match(regex);
+// console.log(result) // [cat, bat]
+
+text = "Somos123";
+regex = /[a-z0-9]/ig;
+result = text.match(regex);
+// console.log(result);  // ['S','o','m','o','s','1','2','3']
+
+text = "Blueberry 3.141592653s are delicious";
+regex = /[h-s2-6]/ig;
+result = text.match(regex);
+// console.log(result);  // ['l','l','r','3','4','5','2','6','5','3','r','l','i','i','o','s']
+
+text = "3 blind mice.";
+regex = /[^aeiou0-9]/ig;
+result = text.match(regex);
+// console.log(result);  // ['','b','l','i','n','d','','m','i','c','e','.']
+
+text = "Cal and Ricky both like racing";
+regext = /^Cal/;    // busca la palabra Cal al inicio del texto
+result = regex.test(text);
+// console.log(result);  // true
+
+text = "Sometimes a story will have to end";
+regex = /end$/;   // busca la palabra end al final del texto
+result = regex.test(text);
+// console.log(result);  // true
+
+text = "Mississippi";
+regex = /s+/ig; // todas las s que estén una o más veces
+result = text.match(regex);
+// console.log(result);    // ['ss', 'ss']
+
+text = "gooooooooal!";
+regex = /go*/g; // las g y las o que se repitan muchas veces
+result = text.match(regex);
+// console.log(result);  // ['goooooooo'] 
+
+/*  
+ *  Lazy and Greedy Match
+ *  La búsqueda Greedy encuentra la parte más larga que satisface el patrón de
+ *  búsqueda (regex). Ésta se define con el símbolo: *
+ *  La búsqueda Lazy encuentra la parte más pequeña y se define agregando el
+ *  símbolo: ? a la búsqueda greedy.
+ */
+
+text = "<h1>Winter is coming</h1>";
+regex = /<.*?>/g; // Change this line
+result = text.match(regex);
+// console.log(result)
+
+/*  
+ *  Shorthand
+ *  \w    : todas las letras y números
+ *  \W    : todo lo que no sea letras o números
+ *  \d    : todos los números
+ *  \D    : todo lo que no es número
+ *  \s    : espacio en blanco
+ *  \S    : todo menos espacio en blanco
+ */
+
+text = "The five boxing wizards jump quickly.";
+regex = /\w/ig; // solo letras y números en todo el ejemplo
+let counter = text.match(regex).length;
+// console.log(counter) // 31
+
+text = "The five boxing wizards jump quickly.";
+regex = /\W/ig; // todo lo que no sea letra o número
+counter = text.match(regex).length;
+// console.log(counter) // 6
+
+text = "2001: A Space Odyssey";
+regex = /\d/g;  // todo lo que sea número
+counter = text.match(regex).length;
+// console.log(counter); // 4
+
+text = "2001: A Space Odyssey";
+regex= /\D/g; 
+counter = text.match(regex).length; // cuenta los caracteres que no son numeros
+// console.log(counter); // 17
+
+text = "Whitespace. Whitespace everywhere!"
+regex = /\s/g;
+// console.log(text.match(regex)); // [" ", " "]
+
+text = "Whitespace. Whitespace everywhere!"
+regex = /\S/g;
+counter = text.match(regex).length;
+// console.log(counter); // Returns 32
+
+/*  
+ *  Rangos
+ *  Para buscar coincidencias de una letra en cierto rango de veces se usa {}
+ */
+let A4 = "haaaah";
+let A2 = "haah";
+let A100 = "h" + "a".repeat(100) + "h";
+let multipleA = /ha{3,5}h/;
+// console.log(multipleA.test(A4)); // true
+// console.log(multipleA.test(A2)); // false
+// console.log(multipleA.test(A100)); // false
+
+let haStr = "Hazzzzah";
+let haRegex = /Haz{4,}ah/; // 4 o más veces
+result = haRegex.test(haStr);
+// console.log(result);
+
+/*  
+ *  Buscar todos o ninguno
+ *  Al introducir en una palabra el símbolo ? buscará la coincidencia con o sin
+ *  la letra inmediatamente anterior al símbolo.
+ */
+
+text = "favorite color colour";
+regex = /favou?rite|colou?r/g;
+result = regex.test(text);
+// console.log(result);  // true
+// console.log(text.match(regex)); // ['favorite', 'color', 'colour']
+
+
+/*  
+ *  Positive and Negative Lookahead (Búsqueda hacia adelante)
+ *  Los lookaheads son patrones que permiten hacer la búsqueda hacia adelante en
+ *  la cadena de texto. Los dos tipos de Lookaheads que hay son:
+ *  ?=... : busca que el texto puesto después del = exista.
+ *  ?!... : busca que el texto luego del ! no exista.
+ */
+let password = "abc123";
+let checkPass = /(?=\w{3,6})(?=\D*\d)/;
+// console.log(checkPass.test(password)); // true
+
+password = "che123";
+checkPass = /^\D(?=\w{5})(?=\w*\d{2})/; // no num start, 5 char long, 2 numbers
+// console.log(checkPass.test(password));
+
+/*  
+ *  Mixed Grouping
+ *
+ */
+text = "Pumpkin";
+regex = /P(engu|umpk)in/;
+result = regex.test(text);
+// console.log(result); // true
+
+/*  
+ *  Capture groups
+ *  Para buscar cadenas repetidas a lo largo de un texto, se puede usar los
+ *  paréntesis, para no tener que repetir el código regex para entrar cada
+ *  coincidencia de cada grupo de caracteres.
+ *  Para especificar donde aparece cada coincidencia se usa una barra invertida
+ *  y un número que va en aumento según los subgrupos de la cadena de texto.
+ *
+ */
+text = "regex regex";
+regex = /(\w+)\s\1/;
+// console.log(regex.test(text)); // true
+// console.log(text.match(regex)); // ['regex regex', 'regex']
+
+text = "42 42 42";
+regex = /^(\d+)\s\1\s\1$/;
+result = text.match(regex);
+// console.log(result);
+
+text = "one two three";
+regex = /(\w+)\s(\w+)\s(\w+)/; // Captura cada palabra
+let replaceText = "$3 $2 $1"; // Cambia el orden de las palabras
+result = text.replace(regex, replaceText);
+// console.log(result);  // "three two one"
+
+text = "   Hello, World!  ";
+regex = /^\s+|\s+$/g; // captura cada espacio al principio y final
+result = text.replace(regex,""); // cambia/borra los espacios
+// console.log(result);    // Hello, World!
+
+
+//  ---------------------------------------------------------------------------
+//  BASIC DATA STRUCTURES
+//  ---------------------------------------------------------------------------
+
+/*  
+ *  Arrays (Arreglos)
+ *  Hay arreglos de dos tipos:
+ *  Simples: contienen una serie de datos de cualquier tipo.
+ *  Complejos: contiene datos y otros arreglos, además puede contener otros
+ *  objetos propios del lenguaje.
+ */
+let simpleArray = ['one', 2, 'three', false, true, undefined, null ];
+let complexArr = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  ['hello', 2, 'cuarentena'],
+  [
+    {
+      nulo: null,
+      verdad: true
+    }
+  ]
+];
+
+// Para cambiar un dato del arreglo se debe hacer accediendo a la posición en la
+// que se encuentra el dato a cambiar.
+simpleArray[3] = 'falso';
+// console.log(simpleArray); // ['one', 2, 'three', 'falso', true, undefined, null ];
+
+/*  
+ *  Añadir elementos a los arreglos
+ *  .push(elementos) : añade los elementos al final.
+ *  .unshift(elementos) : añade los elementos al principio.
+ */
+simpleArray.unshift('cero', 'minus');
+simpleArray.push('another', 'string');
+// console.log(simpleArray);
+
+/*  
+ *  Eliminar elementos de los arreglos
+ *  pop() : elimina el último elemento.
+ *  shift() : elimina el primer elemento.
+ *  splice(index,cantidad) : elimina varios elementos de manera consecutiva, en
+ *  primer lugar se define la posición desde donde se empieza (index), y luego
+ *  la cantidad de elementos que se quieren eliminar (cantidad).
+ *
+ */
+let greetings = ['whats up?', 'hello', 'see ya!', 'hi'];
+greetings.pop(); // ['whats up?', 'hello', 'see ya!']
+greetings.shift(); // ['hello', 'see ya!']
+
+let shifted = greetings.shift(); // return 'hello'
+
+let array = ['today', 'was', 'not', 'so', 'great'];
+array.splice(2, 2); // ['today', 'was', 'great']
+
+let array2 = ['I', 'am', 'feeling', 'really', 'happy'];
+let newArray = array2.splice(3, 2); // newArray equals ['really', 'happy']
+
+// splice(startIndex, amountToDelete, elementsToAdd) 
+// Esta función añade los elementos que se ingresen en tercer lugar.
+const numbers = [10, 11, 12, 12, 15];
+const startIndex = 3;
+const amountToDelete = 1;
+
+numbers.splice(startIndex, amountToDelete, 13, 14);
+// console.log(numbers);   // returns [ 10, 11, 12, 13, 14, 15 ]
+
+/*  
+ *  Copiar elementos de un arreglo
+ *  slice(startIndex, endIndex)
+ *  Al copiar los elementos se crea un nuevo arreglo y el arreglo original sigue
+ *  siendo el mismo.
+ *
+ */
+let forecast = arr => arr.slice(2,4);
+// console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+/*  
+ *  Spread operator: ...arr
+ *  Se usa para copiar todos los elementos de un arreglo y para combinar varios
+ *  arreglos.
+ *  Copiar : arrCopy = [...arr]
+ *  Combinar : newArr = [..., elemento, ...arr, elemento, ...]
+ *
+ */
+let copyMachine = (arr, num) => {
+  let arrCopy = [];
+  while (num >= 1) {
+    arrCopy.push([...arr])
+    num--;
+  }
+  return arrCopy
+} 
+// console.log(copyMachine([true, false, true],2));
+
+let combineArrs = (arr1, arr2) => [...arr1, ...arr2];
+// console.log(combineArrs(['Paris', 'Madrid'], ['France', 'Spain']));
+
+
+/*  
+ *  Buscar la existencia de un elemento
+ *  Para buscar si un elemento existe en un arreglo se hace por medio del método
+ *  indexOf(). Éste método toma un elemento como parámetro y lo busca en el
+ *  arreglo donde se defina el método.
+ *
+ */
+let quickCheck = (arr, elem) => arr.indexOf(elem) >= 0;
+// console.log(quickCheck(['one', 'two', 'three', 'four'], 'four'));
+
+/*  
+ *  Objetos
+ *  Los objetos son colecciones de pares de datos, construidos con un
+ *  identificador (key) y su valor (value).
+ *  object = { key: value, key: value, ... }
+ *
+ */
+let foods = { 
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  vegetables: {
+    aparagus: 12,
+    carrots: 6,
+    broccoli: 16
+  }
+}
+
+foods['bananas'] = 13;    // añade la entrada bananas: 13.
+foods.vegetables.carrots = 12;  // edita el valor de un elemento.
+let value = foods['apples'];  // toma el valor del identificador definido.
+delete foods.plums;   // borra el elemento y su valor.
+
+// Para buscar si una propiedad existe en el objeto
+foods.hasOwnProperty('apples'); // true
+// console.log('apples' in foods); // true
+
+// Iterar en un objeto con for...in
+for (let food in foods) {
+  // console.log(food);
+}
+
+// Para generar un arreglo con elementos de un objeto se usa Object.keys()
+let arrObj = Object.keys(foods);
+// console.log(arrObj);
+
+//  ---------------------------------------------------------------------------
+//  OBJECT ORIENTED PROGRAMMING
+//  ---------------------------------------------------------------------------
+
+/*  
+ *  Objetos
+ *  objeto = { propiedad : valor,... }
+ *
+ */
+let dog = {
+  name: 'Rafa',
+  numLegs: 4
+};
+let dogName = dog.name;       // así se accede al valor de cualquier propiedad
+
+
+/*  
+ *  Métodos
+ *  Los métodos son propiedades de los objestos que son funciones.
+ *
+ */
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+  sayName: function() {return "The name of this duck is " + this.name + ".";}
+};
+duck.sayName();   // devuelve: "The name of this duch is Aflac."
+
+/*  
+ *  Constructores (Constructor Function)
+ *  Son funciones que crean nuevos objetos. Definen las propiedades y los
+ *  comportamientos de estos nuevos objetos. Son como maquetas para la creación
+ *  de nuevos objetos.
+ *
+ */
+function Bird() {
+  this.name = "Albert";
+  this.color = "blue";
+  this.numLegs = 2;
+}
+let blueBird = new Bird();  // { name: 'Albert', color: 'blue', numLegs: 2 }
+
+function Dog(name, color) {
+  this.name = name;
+  this.color = color;
+}
+let redDog = new Dog('Rafa', 'red');  // { name: 'Rafa', color: 'red'}
+
+/*  
+ *  Para verificar si un objeto fue creado por un constructor se utiliza la
+ *  función instanceof. Declarando así: objeto instanceof constructor.
+ *  Si el objeto fue creado por un constructor la declaración da como resultado
+ *  true, de lo contrario false.
+ *
+ */
+let crow = new Bird();
+let canary = {
+  name: "Mildred",
+  color: "Yellow",
+  numLegs: 2
+};
+
+crow instanceof Bird; // true
+canary instanceof Bird; // false
+canary.constructor === Bird; // false. constructor es una alternativa a instanceof
+
+/*  
+ *  Cuando se tiene una propiedad que se repite en cada uno de los objetos que
+ *  se crean se usa la función prototype para evitar la duplicación de la
+ *  propiedad en todo el código.
+ *
+ */
+Dog.prototype.numLegs = 4;
+let beagle = new Dog("Snoopy", "Black&White");
+
+
+//  ---------------------------------------------------------------------------
+//  FUNCTIONAL PROGRAMMING
+//  ---------------------------------------------------------------------------
+
+/*  
+ *  La programación funcional es un paradigma de programación declarativa, es
+ *  decir, una forma de afrontar el mismo problema de vista diferente, cuyo
+ *  objetivo es centrarse en describir qué hacer, y no cómo hacerlo.
+ *
+ */
+
+/*  
+ *  First Class Functions
+ *  Son funciones que se tratan como cualquier otra variable, pueden ser usadas
+ *  como argumento en otras funciones, ser retornadas por otra función o ser
+ *  asignadas a una variable.
+ */
+const foo = () => console.log("foobar");
+foo();
+
+const plus1 = a => a + 1;
+plus1(4); // 5
+
+
+/*  
+ *  Pure Functions
+ *  Una función pura es la que para el mismo input, siempre genera el mismo
+ *  output, y no tiene efectos secundarios (cambios no relacionados con la
+ *  función). Además usarlas ayuda a reducir líneas de código y evitar la
+ *  modificación de arreglos y objetos.
+ */
+let x = 1, y = 2;
+const add = () => x + y;    // Función impura
+add();    // 3
+
+const pureAdd = (x,y) => x + y;
+pureAdd(1, 3);  // 4
+
+// Impura
+const getDayOfWeek = () => {
+  let d = new Date();
+  console.log(d.getDay());
+}
+getDayOfWeek();
+
+// Pura
+const getDayPure = d => console.log(d.getDay());
+getDayPure(new Date());
+
+// Impura
+let aValue = [0];
+const impureFn = (varByRef) => varByRef.push(1);
+impureFn(aValue); // aValue se convierte en [0, 1]
+
+// Pura
+let anotherValue = [0];
+const pureFn = (varByRef) => varByRef.concat(1);
+let newArray = pureFn(anotherValue);
+// anotherValue sigue igual
+// Al usar concat se crea un nuevo arreglo
+
+// Referencial Transparency
+const addRF = (a, b) => a + b;
+addRF(addRF(1, 3), addRF(6, 2)); // 12
+addRF(addRF(1, 3), 8);  // 12
 
 switch (new Date().getDay()) {
   case 0:
